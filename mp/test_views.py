7e832187185360a1e74c127ed59818a9dc5d7def
MichaelSingh7 <mikael.1@live.co.uk>
2020-06-05 15:29:35 +0000

    Complete 4 tests on test_views.py

---
+++ mp/test_views.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from .models import Item
+
+
+class TestViews(TestCase):
+    def test_get_home_page(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "mp_list.html")
+
+    def test_get_add_item_page(self):
+        page = self.client.get("/add")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "item_form.html")
+
+    def test_get_edit_item_page(self):
+        item = Item(name="Create A Test")
+        item.save()
+
+        page = self.client.get("/edit/{0}".format(item.id))
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "item_form.html")
