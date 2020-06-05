1fb37284633c106ced5676b2afeac4b17b397b5e
MichaelSingh7 <mikael.1@live.co.uk>
2020-06-05 16:00:27 +0000

    Updated test get item for page that does not exist

--- mp/test_views.py
+++ mp/test_views.py
@@ -20,3 +20,7 @@ class TestViews(TestCase):
         page = self.client.get("/edit/{0}".format(item.id))
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "item_form.html")
+
+    def test_get_edit_item_for_page_that_does_not_exist(self):
+        page = self.client.get("/edit/1")
+        self.assertEqual(page.status_code, 404)
