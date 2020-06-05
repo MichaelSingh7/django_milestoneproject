49e3b5caac60ef202d3382487d30c2c0837fbd44
MichaelSingh7 <mikael.1@live.co.uk>
2020-06-05 11:40:32 +0000

    Updates made to allow user to toggle and edit items

--- django_milestoneproject/urls.py
+++ django_milestoneproject/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from mp.views import get_mp_list
+from mp.views import get_mp_list, create_an_item, edit_an_item, toggle_status
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', get_mp_list)
+    url(r'^$', get_mp_list),
+    url(r'^add$', create_an_item),
+    url(r'^edit/(?P<id>\d+)$', edit_an_item),
+    url(r'^toggle/(?P<id>\d+)$', toggle_status)
 ]
