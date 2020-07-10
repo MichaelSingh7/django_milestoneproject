f83a0025f3506265be8d3e6b5d4a0a689f9d5b66
MichaelSingh7 <mikael.1@live.co.uk>
2020-07-10 21:23:39 +0000

    Updated settings.py so user can reset password

--- django_milestoneproject/settings.py
+++ django_milestoneproject/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIR = (
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.live.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-EMAIL_PORT = 25
+EMAIL_PORT = 587
